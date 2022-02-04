928f4bd31952a1622d4908be286108d38e502daf
eneskrdg <36544987+eneskrdg@users.noreply.github.com>
2022-02-04 22:56:01 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  useColorMode,
+  useColorModeValue,
+  Text,
+  Select,
+  Grid,
+  GridItem,
+  Box,
+  Spacer,
+} from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Meals from "./pages/meals";
+import Upgrade from "./pages/upgrade";
+import Payments from "./pages/payment";
+
+
+function App() {
+
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+     
+      
+        <Route path="/meals" element={<Meals />}/>
+        <Route path="/upgrade" element={<Upgrade />}/>
+        <Route path="/payment" element={<Payments />}/>
+    
+    </Routes>
+    
+  </Router>
+    
+  );
+}
+
+export default App;
